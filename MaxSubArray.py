9e3dbdb95df09c3d99ab80d29db54fff60fa0658
Krish Shah <114970376+kri-shah@users.noreply.github.com>
2025-07-19 16:13:50 -0400

    Create MaxSubArray.py

---
+++ MaxSubArray.py
@@ -0,0 +1,11 @@
+#accidentally derived Kadane's Alg
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxsum = float('-inf')
+        curmax = float('-inf')
+        for i in nums:
+            curmax = max(i, curmax + i)
+            maxsum = max(maxsum, curmax)
+        
+        return maxsum
