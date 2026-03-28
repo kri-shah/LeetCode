ec77e214948c174d8d74a6b020b1d4b156e66a29
Krish Shah <114970376+kri-shah@users.noreply.github.com>
2026-03-28 14:26:25 -0400

    Create MinimumFallingPathSum.py

---
+++ MinimumFallingPathSum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        ROWS = len(matrix)        
+        COLS = len(matrix[0])
+
+        dp = matrix[0][:]
+        
+        for r in range(1, ROWS):
+            curr = [0] * ROWS
+            for c in range(COLS):
+                if c == 0:
+                    curr[c] = matrix[r][c] + min(dp[c], dp[c + 1])
+                elif c == COLS - 1:
+                    curr[c] = matrix[r][c] + min(dp[c], dp[c - 1])
+                else:
+                    curr[c] = matrix[r][c] + min(dp[c], dp[c - 1], dp[c + 1])
+            dp = curr[:]
+        
+        return min(dp)
